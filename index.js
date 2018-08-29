ac2f94bab511994527971900943b4c1e7bf8dd0d
Marissa So <sotom591@gmail.com>
2018-08-29 17:20:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,18 +5,18 @@ function happyHolidays()
 
 function happyHolidaysTo()
 {
-  console.log("Happy holidays, ${name}!]");
+  return ("Happy holidays, ${name}!]");
 }
 happyHolidaysTo ('Jack');
 
 function happyHolidayTo()
 {
-  console.log("Happy ${holiday}, $name}!");
+   return ("Happy ${holiday}, $name}!");
 }
 happyHolidayTo('Kwanzaa', 'John');
 
 function holidayCountdown()
 {
-  console.log("It's ${days} days until ${holiday}!");
+  return ("It's ${days} days until ${holiday}!");
 }
 holidayCountdown(5, "Christmas");
